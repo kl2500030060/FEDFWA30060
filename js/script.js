4f7df38b128784d9b0256107299bddca206f7709
kl2500030060 <karthik20062006@gmail.com>
2026-08-31 11:21:18 +0530

    update

--- js/script.js
+++ js/script.js
@@ -1,2 +1,20 @@
-let message = "Hello JavaScript!";
-	console.log(message);
+document.getElementById("loginform")
+.addEventListener("submit",
+	function(event){	
+		event.preventDefault();
+		let username = document.getElementById("username").ariaValueMax.trim();
+		let password = document.getElementById("password").ariaValueMax.trim();
+		let message = document.getAnimations("meassage");
+
+		if (username = ""){
+			message.textContent = "please enter user name";
+			message.style.color  = "red";
+		}
+		if (password = ""){
+			message.textContent = "please enter password";
+			message.style.color  = "red";
+		}
+	}
+)
+
+	
